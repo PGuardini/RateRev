1faa788b6a1b877e2fca2e46db163c2ed9def88f
Victor Hugo <victorhugonass@gmail.com>
2018-03-31 00:02:05 -0300

    zoom "funcionando", mas ainda precisa de alguns detalhes

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ include "html/cabecalho.html";
                 <img src="imagens/gta-v-300x200.jpg">
                 <div class="text-item">
                     <h2>Título da imagem</h2>
+                    <div class="ui rating" data-max-rating="5"></div>
                 </div>
             </a>
             </div>
@@ -18,6 +19,8 @@ include "html/cabecalho.html";
                 <img src="imagens/gta-v-300x200.jpg">
                 <div class="text-item">
                     <h2>Título da imagem</h2>
+
+
                 </div>
             </a>
             </div>
@@ -26,13 +29,74 @@ include "html/cabecalho.html";
                 <img src="imagens/gta-v-300x200.jpg">
                 <div class="text-item">
                     <h2>Título da imagem</h2>
+
+
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="three column row">
+        <div class=" column zoom">
+            <a href="#">
+                <img src="imagens/gta-v-300x200.jpg">
+                <div class="text-item">
+                    <h2>Título da imagem</h2>
+
+
                 </div>
             </a>
             </div>
+        <div class=" column zoom">
+            <a href="#">
+                <img src="imagens/gta-v-300x200.jpg">
+                <div class="text-item">
+                    <h2>Título da imagem</h2>
+
+                </div>
+            </a>
+            </div>
+        <div class=" column zoom">
+            <a href="#">    
+                <img src="imagens/gta-v-300x200.jpg">
+                <div class="text-item">
+                    <h2>Título da imagem</h2>
+
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="three column row">
+        <div class=" column zoom">
+            <a href="#">
+                <img src="imagens/gta-v-300x200.jpg">
+                <div class="text-item">
+                    <h2>Título da imagem</h2>
+
+                </div>
+            </a>
+            </div>
+        <div class=" column zoom">
+            <a href="#">
+                <img src="imagens/gta-v-300x200.jpg">
+                <div class="text-item">
+                    <h2>Título da imagem</h2>
+
+                </div>
+            </a>
+            </div>
+        <div class=" column zoom">
+            <a href="#">    
+                <img src="imagens/gta-v-300x200.jpg">
+                <div class="text-item">
+                    <h2>Título da imagem</h2>
+
+                </div>
+            </a>
+        </div>
     </div>
 </div>
 
 <div class="clear"><div>
 <?
-//include "html/rodape.html";
+include "html/rodape.html";
 ?>
