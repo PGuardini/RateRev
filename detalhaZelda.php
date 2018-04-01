3bc68af84031f84349cfabe045dcb7e450477193
Pedro Augusto <pedro.canteli2001@gmail.com>
2018-03-31 23:12:50 -0300

    inclusão de favicon e criação da parte inferior das páginas DetalhaJogo. Novas fontes foram incluídas e tons similares da paleta combinada foram utilizados

---
+++ detalhaZelda.php
@@ -0,0 +1,67 @@
+<?php
+    include("html/cabecalho.html");
+?>
+    
+
+    <h2 class="ui horizontal divider header tituloSegmento">
+        Resenhas
+    </h2>
+
+    <div class="ui grid">
+        <div class="three wide column"></div>
+        <div class="one wide column">
+            <div class="espacoUsuario">
+                <img class="ui circular big image" src="imagens/detalhaJogo/avatares/avatar1.png">
+            </div>
+            <section class="username">@violeta</section>
+        </div>
+        <div class="ten wide column">
+            <div class="talk-bubble tri-right left-in">
+                <div class="talktext">
+                    <h3 class="tituloResenha">Tudo o que você faz, é por conta própria!</h3>
+                    <div class="ui divider"></div>
+                    <div class="espacoComecoPost"></div>
+                    <p>Sou um grande fã da franquia The Legend of Zelda e, como tal, há vários anos tenho me decepcionado com os jogos da série. Não por serem ruins – muito longe disso – mas por terem se limitado sempre às mesmas experiências e caído nas mesmas armadilhas que outros games da indústria têm tido há muito tempo.</p>
+                    <p>O jogo original, de 1986, quebrou paradigmas na época, trazendo diversas novidades para o mercado. Nele, você era colocado em um mundo totalmente aberto, sem um caminho certo. Bastava escolher um lado e começar a explorar. Sem tutorial, sem roteiros pré-definidos. Apenas você, sua curiosidade e sua coragem.</p>
+                    <p>O primeiro jogo é um dos meus preferidos da série, então naturalmente fiquei interessado quando The Legend of Zelda: Breath of the Wild foi anunciado, já que a Nintendo prometeu retornar às origens da franquia ao mesmo tempo que quebraria suas amarras estabelecidas desde A Link to The Past.</p>
+                    <p>Propor é fácil, mas fugir de padrões de game design estabelecidos há mais de 20 anos na franquia e até mesmo de toda uma indústria não é um trabalho simples. Sempre fiquei com um pé atrás com essa promessa… No entanto, depois de jogar, surpreendentemente descobri que a Nintendo realmente conseguiu.</p>
+                    <div class="espacoFimPost"></div>
+                    <div class="ui divider"></div>
+                    <div class="ui grid">
+                        <div class="nine wide column grid">
+                            <div class="ui label corLabelLaranja">
+                                <i class="thumbs down icon"></i>
+                                    5
+                            </div>
+                            <div class="ui label corLabelLaranja">
+                                <i class="thumbs up icon"></i>
+                                    6
+                            </div>
+                            <div class="ui label corLabelAzul">
+                                <i class="comments icon"></i>
+                                    5 comentários
+                            </div>
+                            <div class="ui label corLabelAzul">
+                                <i class="calendar alternate icon"></i>
+                                    Postado há 3 dias
+                            </div>
+                        </div>
+
+                        <div class="right floated four wide column grid">
+                            <div class="ui label corLabelLaranja">
+                                <i class="search plus icon"></i>
+                                    Ver post completo
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="one wide column"></div>
+    </div>
+
+    <div class="espacoAntesRodape">.</div>
+
+<?php
+    include("html/rodape.html");
+?>
